6879e0662cb8d3585c2888158261d4ac5fad059b
David Jacobs <djacobs@gmail.com>
2015-01-12 08:21:33 -0500

    update contact information

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    author = 'Simon Whitaker',
-    author_email = 'simon@goosoftware.co.uk',
+    author = 'David Jacobs',
+    author_email = 'david@29.io',
     description = 'A python library for interacting with the Apple Push Notification Service',
-    download_url = 'http://github.com/simonwhitaker/PyAPNs',
+    download_url = 'https://github.com/djacobs/PyAPNs',
     license = 'unlicense.org',
     name = 'apns',
     py_modules = ['apns'],
     scripts = ['apns-send'],
-    url = 'http://www.goosoftware.co.uk/',
+    url = 'http://29.io/',
     version = '2.0.1',
 )
